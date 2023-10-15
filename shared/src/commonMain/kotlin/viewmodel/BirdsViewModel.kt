21bff238a71deaaf067451884e2f847215bb4f28
joNA <jitzian@gmail.com>
2023-10-15 00:27:38 -0400

    kmm jc - First iteration for creating VM and logic for presenting / filtering data for UI

---
+++ shared/src/commonMain/kotlin/viewmodel/BirdsViewModel.kt
@@ -0,0 +1,58 @@
+package viewmodel
+
+import dev.icerock.moko.mvvm.viewmodel.ViewModel
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import model.BirdImage
+
+data class BirdsUIState(
+    val images: List<BirdImage> = emptyList(),
+    val selectedCategory: String? = null,
+) {
+    val categories = images.map { it.category }.toSet()
+    val selectedImages = images.filter { it.category == selectedCategory }
+}
+
+class BirdsViewModel : ViewModel() {
+    private val _state = MutableStateFlow(BirdsUIState())
+    val state = _state.asStateFlow()
+
+    private val httpClient: HttpClient = HttpClient {
+        install(ContentNegotiation) {
+            json()
+        }
+    }
+
+    init {
+        updateImages()
+    }
+
+    override fun onCleared() {
+        httpClient.close()
+    }
+
+    fun selectCategory(category: String) {
+        _state.update { it.copy(selectedCategory = category) }
+    }
+
+    fun updateImages() {
+        viewModelScope.launch {
+            _state.update { it.copy(images = getImages()) }
+        }
+    }
+
+    private suspend fun getImages(): List<BirdImage> {
+        return httpClient
+            .get("https://sebi.io/demo-image-api/pictures.json")
+            .body()
+    }
+
+
+}
